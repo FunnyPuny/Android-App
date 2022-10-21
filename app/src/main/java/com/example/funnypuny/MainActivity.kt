2e4f28941fbb0576f06c1687478a641f500bc2ca
farida <farida.hairullina5@gmail.com>
2022-10-22 01:57:25 +0300

    Create NavigationBar and ToolBar

--- app/src/main/java/com/example/funnypuny/MainActivity.kt
+++ app/src/main/java/com/example/funnypuny/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.funnypuny
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var bottom_navigation: BottomNavigationView
+    private lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation = findViewById(R.id.bottom_navigation_main)
+        imageView = findViewById(R.id.iv_add)
+
+        bottom_navigation.itemIconTintList = null
+        imageView.setOnClickListener(this)
     }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.iv_add ->{
+                Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
 }
