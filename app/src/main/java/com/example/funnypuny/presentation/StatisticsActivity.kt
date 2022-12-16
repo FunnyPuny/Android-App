4b94058f79ea765344b95272bec71ad16c1bf0f3
farida <farida.hairullina5@gmail.com>
2022-12-16 15:59:35 +0100

    Add Progress Bar

--- app/src/main/java/com/example/funnypuny/presentation/StatisticsActivity.kt
+++ app/src/main/java/com/example/funnypuny/presentation/StatisticsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
 import com.example.funnypuny.R
 import com.example.funnypuny.databinding.ActivityHabitItemBinding
 import com.example.funnypuny.databinding.ActivityStatisticsBinding
@@ -21,6 +22,7 @@ class StatisticsActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.statistics_container, StatisticsFragment())
             .commit()
+
     }
 
     companion object {
--- app/src/main/java/com/example/funnypuny/presentation/StatisticsFragment.kt
+++ app/src/main/java/com/example/funnypuny/presentation/StatisticsFragment.kt
@@ -1,17 +1,25 @@
 package com.example.funnypuny.presentation
 
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.ThemedSpinnerAdapter
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import com.example.funnypuny.R
 import com.example.funnypuny.databinding.FragmentHabitItemBinding
 import com.example.funnypuny.databinding.FragmentStatisticsBinding
 
 
 class StatisticsFragment : Fragment() {
+
+    private var status = 0
+    private val handler = Handler()
 
     private var _binding: FragmentStatisticsBinding? = null
     private val binding: FragmentStatisticsBinding
@@ -23,6 +31,33 @@ class StatisticsFragment : Fragment() {
     ): View {
         _binding = FragmentStatisticsBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val drawable = resources.getDrawable(R.drawable.circularprogressbar, null)
+        //val drawable = ContextCompat.getDrawable(requireActivity(), R.drawable.circularprogressbar)
+        //val drawable = ResourcesCompat.getDrawable(getResources(),R.drawable.circularprogressbar, null)
+        //val progressBar: ProgressBar = findViewById(R.id.circularProgressbar)
+        binding.circularProgressbar.progress = 0
+        binding.circularProgressbar.secondaryProgress = 0
+        binding.circularProgressbar.max = 100
+        binding.circularProgressbar.progressDrawable = drawable
+        Thread {
+            while (status < 100) {
+                status += 1
+                handler.post {
+                    binding.circularProgressbar.progress = status
+                    binding.textView.text = String.format("%d%%", status)
+                }
+                try {
+                    Thread.sleep(16)
+                }
+                catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }.start()
     }
 
     override fun onDestroyView() {
